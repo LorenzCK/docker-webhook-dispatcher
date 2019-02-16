cfc895fc675240327e87ae32c0545bdd7eed436d
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-02-16 23:52:02 +0100

    Verify requests using secret

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const crypto = require('crypto');
 const minimatch = require('minimatch');
 const { spawnSync } = require('child_process');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    'limit': '25MB',
+    'verify': function(req, res, buf, encoding) {
+        if(process.env.WEBHOOK_DISPATCHER_SECRET) {
+            var requestSignature = req.get('X-Hub-Signature');
+            if(!requestSignature) {
+                console.error('Request does not include X-Hub-Signature header');
+                throw "Missing header";
+            }
+
+            var hash = crypto.createHmac('sha1', process.env.WEBHOOK_DISPATCHER_SECRET);
+            var expectedSignature = 'sha1=' + hash.update(buf, encoding).digest('hex');
+            
+            req.verified = crypto.timingSafeEqual(Buffer.from(requestSignature), Buffer.from(expectedSignature));
+        }
+        else {
+            req.verified = true;
+        }
+    }
+}));
 
 const WEB_HOST = process.env.WEB_HOST || '0.0.0.0';
 const WEB_PORT = process.env.WEB_PORT || 8080;
@@ -64,7 +84,11 @@ if(hookMap.length == 0) {
 console.log('Registered ' + hookMap.length + ' hooks:');
 hookMap.forEach(element => {
     console.log(JSON.stringify(element));
-})
+});
+
+if(!process.env.WEBHOOK_DISPATCHER_SECRET) {
+    console.error('Environment variable WEBHOOK_DISPATCHER_SECRET not set, will not verify requests');
+}
 
 app.post('*', function(req, resp) {
     if(!req.get('User-Agent').includes('GitHub-Hookshot')) {
@@ -75,6 +99,12 @@ app.post('*', function(req, resp) {
 
     const repoId = req.body.repository.id;
     const hookEvent = req.get('X-GitHub-Event');
+
+    if(!req.verified) {
+        console.error('Request not verified');
+        resp.sendStatus(401);
+        return;
+    }
 
     if(repoId && hookEvent) {
         console.log('Webhook payload: ' + JSON.stringify(req.body));
